959f724168fd1ea8f2d23c1de2d318e147498a18
Gabrielle Madden <49839049+gabriellemadden@users.noreply.github.com>
2019-10-16 20:54:27 -0400

    Update SceneAnimator.cs

--- SceneAnimator.cs
+++ SceneAnimator.cs
@@ -6,10 +6,7 @@ public class SceneAnimator : MonoBehaviour
 {
     public GameObject layer0, layer1, layer2, layer3, canvas;
     public float scalingFactor = 0.1f;
-    public float maxTransform = 125.0f;
 
-    public float moveTime = 1.0f;
-    float moveTimer;
     float scale;
     Vector3 oldMousePos;
     Vector3 posOldRelative;
@@ -50,55 +47,6 @@ public class SceneAnimator : MonoBehaviour
         layer2.transform.position = l2 + can;
         layer3.transform.position = l3 + can;
 
-    }
-
-    //void Start()
-    //{
-    //    moveTimer = moveTime;
-    //    oldMousePos = Input.mousePosition;
-    //}
-
-
-    //void Update()
-    //{
-    //    //if mouse moves, want image to move. if mouse STOPS, want timer to run. 
-    //    if (Mathf.Approximately(Input.GetAxis("Mouse X"), 0.0f) ||
-    //        Mathf.Approximately(Input.GetAxis("Mouse Y"), 0.0f))
-    //    {
-    //        moveTimer -= Time.deltaTime;
-    //        if (moveTimer < 0)
-    //            moveTimer = 0;
-    //    }
-    //    else moveTimer = moveTime;
-
-    //    Vector3 mousePos = Input.mousePosition;
-    //    if (mousePos != oldMousePos) posOldRelative = mousePos - oldMousePos;
-
-    //    scale = scalingFactor * moveTimer;
-
-    //    if (posOldRelative.magnitude > 10.0f) posOldRelative = posOldRelative.normalized*10;
-
-    //    Vector3 currentTransform = new Vector3(0, 0, 0);
-    //    currentTransform.x = splash.transform.position.x + posOldRelative.x * scale - 625;
-    //    currentTransform.y = (splash.transform.position.y + posOldRelative.y * scale - 350)*1.25f;
-    //    //Debug.Log("Transform = " + currentTransform.x.ToString() + ", " + currentTransform.y.ToString());
-    //    if (Mathf.Abs(currentTransform.x) < maxTransform 
-    //        && Mathf.Abs(currentTransform.y) < maxTransform)
-    //    {
-    //        splash.transform.position += (posOldRelative) * scale;
-    //        map.transform.position += (posOldRelative) * scale * 2;
-    //        buttons.transform.position += (posOldRelative) * scale * 3;
-    //    }
-
-    //    oldMousePos = mousePos;
-    //}
-
-    Vector3 MakePositive(Vector3 vec)
-    {
-        if (vec.x < 0) vec.x *= -1;
-        if (vec.y < 0) vec.y *= -1;
-        if (vec.z < 0) vec.z *= -1;
-        return vec;
     }
 }
 
